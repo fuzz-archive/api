1e54eaff2c44658ad31c8c5c3dd66d411739295c
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2021-09-29 19:51:43 +1000

    tslib and I forgot to add the type stuff

--- src/Models/Meme.ts
+++ src/Models/Meme.ts
@@ -1,6 +1,7 @@
 import { model, Schema } from 'mongoose'
+import type { Meme } from '../Types'
 
-export default model('Meme', new Schema({
+export default model<Meme>('Meme', new Schema({
   id: { type: Number, required: true },
   url: { type: String, required: true }
 }))
--- src/Models/RandomImage.ts
+++ src/Models/RandomImage.ts
@@ -1,6 +1,6 @@
 import { model, Schema } from 'mongoose'
 
-export default model('Random', new Schema({
+export default model('RandomImage', new Schema({
   id: { type: Number, required: true },
   url: { type: String, required: true },
   nsfw: { type: Boolean, required: false, default: false }
